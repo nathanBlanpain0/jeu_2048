fd3303fa278797e0b09429970a2bd0861dc4b595
nathanBlanpain0 <nathanblanpain@gmail.com>
2024-11-26 17:07:26 +0100

    ah2

--- Assets/Scenes/center.cs
+++ Assets/Scenes/center.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class center : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    private Rigidbody2D rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        transform.position = new Vector2(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y));
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Vector2.SqrMagnitude(rb.velocity) < 0.1f)
+        {
+            transform.position = new Vector2(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y));
+        } 
     }
 }
+
