611c3aa6104ed86d229b73749c5a4a12f1a7192e
nathanBlanpain0 <nathanblanpain@gmail.com>
2024-11-26 17:18:20 +0100

    ah3

--- Assets/Scenes/danse.cs
+++ Assets/Scenes/danse.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class danse : MonoBehaviour
 {
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Input.getKey("up"))
+        {
+            rb.velocity = new Vector2(0, 2);
+        }
+        if (Input.getKey("down"))
+        {
+            rb.velocity = new Vector2(0, -2);
+        }
+        if (Input.getKey("right"))
+        {
+            rb.velocity = new Vector2(2, 0);
+        }
+        if (Input.getKey("left"))
+        {
+            rb.velocity = new Vector2(-2, 0);
+        }
     }
 }
